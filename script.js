82022c1b5e1a806c467a364083d5198aad11622f
brenodacruz <brenohenriquecruz22@gmail.com>
2024-03-15 16:09:49 -0300

    Coloquei o site responsivo com menu

--- script.js
+++ script.js
@@ -61,3 +61,18 @@ function direita() {
     
     
 }
+
+let btnmenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay')
+
+btnmenu.addEventListener('click', ()=> {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=> {
+    menu.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', ()=> {
+    menu.classList.remove('abrir-menu')
+})
